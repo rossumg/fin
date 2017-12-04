994b7c340c4743e2c1d8c66bab83e93b237b2006
rossumg <rossumg@uw.edu>
2017-12-04 11:43:46 -0800

    clean up setup view, Fins to Expenses

--- fin/current.sql
+++ fin/current.sql
@@ -120,7 +120,7 @@ TDPrimaryKey = trim( TDPrimaryKey),
 FiscalMonth = trim( FiscalMonth),
 FiscalYear = trim(FiscalYear);
 
--- fix 1
+-- fix 1, run once
 update activitydetail set Budget_Begin = 
 cast(
 concat(trim(split_str(Budget_Begin, '/', 3)),'-',
@@ -128,7 +128,7 @@ concat(trim(split_str(Budget_Begin, '/', 1))),'-',
 trim(split_str(Budget_Begin, '/', 2))) as date)
 where trim(FiscalMonth)in('01','02','03');
 
--- fix 2
+-- fix 2, run once
 update activitydetail set Budget_End = 
 cast(
 concat(trim(split_str(Budget_End, '/', 3)),'-',
@@ -136,7 +136,7 @@ concat(trim(split_str(Budget_End, '/', 1))),'-',
 trim(split_str(Budget_End, '/', 2))) as date)
 where trim(FiscalMonth)in('01','02','03');
 
--- fix 3
+-- fix 3, run once
 update activitydetail set TranDate1 = 
 cast(
 concat(trim(split_str(TranDate1, '/', 3)),'-',
@@ -171,59 +171,6 @@ case
 		 else ''
        -- end
 end;
-
--- fix 6
-update activitydetail ad set
-ProjectCode = trim( ProjectCode),
-GFA = trim( GFA),
-AdjustmentLoadedDate = trim( AdjustmentLoadedDate),
-TranAmount = trim( TranAmount),
-BudgetNbr = trim( BudgetNbr),
-BudgetName = trim( BudgetName),
-AccountCode = trim( AccountCode), 
-TranDescMod = trim( TranDescMod),
-TranReference1 = trim( TranReference1),
-TranReference2 = trim( TranReference2),
-TranReference3 = trim( TranReference3),
-TranReference4 = trim( TranReference4),
-Budget_Begin = trim( Budget_Begin),
-Budget_End = trim( Budget_End),
-TranDate1 = trim( TranDate1),
-Modified = trim( Modified),
-PCAProjectCodeOrig = trim( PCAProjectCodeOrig),
-PCAProjectCodePosting = trim( PCAProjectCodePosting),
-TDPrimaryKey = trim( TDPrimaryKey),
-FiscalMonth = trim( FiscalMonth),
-FiscalYear = trim(FiscalYear);
-
--- fix 7
-update activitydetail ad set ProjectCode = 
-case 
-  when PCAProjectCodePosting='99999Z'
-  or PCAProjectCodePosting='CODEME'
-  or PCAProjectCodePosting='99999Y' then 
-	( select distinct Project_Code from Project_Codes pc  
-      where 1=1
-      and pc.Project_Code = ad.PCAProjectCodeOrig
-	  and pc.BudgetNbr = ad.BudgetNbr
-	  and pc.Project_Status = 'Active')
-  else PCAProjectCodePosting
-end 
--- where trim(TDPrimaryKey) in ('170322039580', '170325035040')
-; 
-
--- fix 8
-update activitydetail set Modified = 
-case 
-  when PCAProjectCodePosting='99999Z' 
-  or PCAProjectCodePosting='CODEME' 
-  or PCAProjectCodePosting='99999Y' then 'Modified'
-  -- else case
-         -- when TranDate1 > @PayDate1 then 'Modified' -- temp out
-		 else ''
-       -- end
-end;
-
 
 
 select 
